02de76206098c2367cb9f32b5a6041b29c795bde
Guinslym <guinslym@gmail.com>
2017-06-10 17:34:03 -0400

    data

--- crontjobs.php
+++ crontjobs.php
@@ -1,7 +1,7 @@
 <?php
 //This script will download all the jobs and 
 //write them in a json file (including the date)
-//
+//2017-06-10-data-en.json
 
 $emplois = array("http://www.ottawacityjobs.ca/en/data/", "http://www.ottawacityjobs.ca/fr/data/"); 
 
